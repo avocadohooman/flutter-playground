7bb49a6bce7ab0f9640796c55da6f952f8109820
avocadohooman <gmolin@student.hive.fi>
2022-10-01 12:57:10 +0200

    adding key screens

---
+++ quizapp/lib/topics/topics.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class Topics extends StatelessWidget {
+  const Topics({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
